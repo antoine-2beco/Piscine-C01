eb35ac28d9a352f6106826f553514747b1fc2d07
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2023-09-08 21:22:32 +0200

    corr08

--- ex08/ft_sort_int_tab.c
+++ ex08/ft_sort_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: ade-beco <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 18:19:34 by ade-beco          #+#    #+#             */
-/*   Updated: 2023/09/08 19:24:35 by ade-beco         ###   ########.fr       */
+/*   Updated: 2023/09/08 21:21:37 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	ft_sort_int_tab(int *tab, int size)
 	con2 = 0;
 	while (con1 != (size - 1))
 	{
-		while (con2 != (size))
+		while ((con2 != (size)) || (con1 != (size - 1)))
 		{
 			a = tab[con1];
 			b = tab[con2];
