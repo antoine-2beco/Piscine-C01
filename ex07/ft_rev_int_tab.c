e17e863b77d9405add799d54d336c30bcac2fd12
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2023-09-08 21:06:05 +0200

    corr07

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -6,21 +6,21 @@
 /*   By: ade-beco <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 14:28:23 by ade-beco          #+#    #+#             */
-/*   Updated: 2023/09/08 10:03:43 by ade-beco         ###   ########.fr       */
+/*   Updated: 2023/09/08 21:05:24 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 void	ft_rev_int_tab(int *tab, int size)
 {
-	int	n_tab[size];
-	int	counter;
+	int	i_size;
+	int	temp;
 
-	counter = 0;
-	while (size != 0)
+	temp = 0;
+	i_size = size;
+	while (size != (i_size / 2))
 	{
-		n_tab[counter] = tab[size - 1];
+		temp = tab[size - 1];
+		tab[size - 1] = tab[i_size - size];
+		tab[i_size - size] = temp;
 		size--;
-		counter++;
 	}
-	tab = n_tab;
 }
